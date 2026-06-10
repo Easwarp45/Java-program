171f3729ed915a74c7bc44a72cd41618abada036
Easwaramurthy <easwarp45@gmail.com>
2026-06-10 14:13:09 +0530

    fix: Correct logic for checking if the array is sorted

--- SortedorNot.java
+++ SortedorNot.java
@@ -8,8 +8,8 @@ public class SortedorNot {
         for (int i = 0; i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
-        for(int i=1;i< arr.length ; i++){
-            if(arr[i] >= arr[i-1]){
+        for(int i=0;i< arr.length-1 ; i++){
+            if(arr[i] >= arr[i+1]){
                 count+=1;
             }
         }
