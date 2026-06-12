0a37efbc43c1f1dab88603d75fbcd3af88704309
Easwaramurthy <easwarp45@gmail.com>
2026-06-12 07:22:58 +0530

    feat: Add RemoveDuplicate program and update DuplicateDisp for improved duplicate handling

--- arrays/DuplicateDisp.java
+++ arrays/DuplicateDisp.java
@@ -8,15 +8,27 @@ class DuplicateDisp{
             arr[i]=sc.nextInt();
         }
         int flag = 0;
-        int j=0;
-        for (int i=0;i<arr.length;i++){
-            if(arr[i] != arr[i+1]){
-                flag =1;
-            }
-            if (flag != 0){
-                continue;
-            }
+        int visiele = arr[0];
+for(int i = 0; i < arr.length; i++) {
+
+    boolean alreadyPrinted = false;
+
+    for(int k = 0; k < i; k++) {
+        if(arr[k] == arr[i]) {
+            alreadyPrinted = true;
+            break;
+        }
+    }
+
+    if(alreadyPrinted){
+        continue;
+    }
+    for(int j = i + 1; j < arr.length; j++) {
+        if(arr[i] == arr[j]) {
+            System.out.print(arr[i] + " ");
+            break;
         }
     }
 }
-  
+}
+}
--- arrays/RemoveDuplicate.java
+++ arrays/RemoveDuplicate.java
@@ -2,6 +2,20 @@ import java.util.*;
 public class RemoveDuplicate {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int num = sc.nextInt();
+        int size = sc.nextInt(); 
+        int arr[]=new int[size];      
+        for (int i= 0;i< size ; i++){
+            arr[i]=sc.nextInt();
+        }
+        int i =0;
+        for ( int j=1;j<size;j++){
+            if( arr[j]!= arr[i]){
+                arr[i+1]=arr[i];
+                i++;
+            }
+        }
+        for (int ii= 0;ii< size ; ii++){
+           System.out.print(arr[ii]+" ");
+        }
     }
 }
