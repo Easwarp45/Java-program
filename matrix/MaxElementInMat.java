b8aa396857a1d4229c7e09c156bc387d66101c66
Easwaramurthy <easwarp45@gmail.com>
2026-06-17 11:50:58 +0530

    give the maximum element in the matrix

---
+++ matrix/MaxElementInMat.java
@@ -0,0 +1,26 @@
+package matrix;
+
+import java.util.Scanner;
+
+public class MaxElementInMat {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int rowcol=sc.nextInt();
+          int mat1[][]=new int[rowcol][rowcol];
+         for(int i=0;i<rowcol;i++){
+            for(int j=0;j<rowcol;j++){
+            mat1[i][j]=sc.nextInt();
+        }
+    }
+    for(int i=0;i<rowcol;i++){
+            int max =mat1[i][0];
+            for(int j=0;j<rowcol;j++){
+                if(mat1[i][j]> max){
+                    max=mat1[i][j];
+                }
+                
+            }
+            System.out.print(max+" ");
+        }
+    }
+}
